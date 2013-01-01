1aa02beb901c6dc086bb68e912b4d7931784b89c
jp <jp@ubuntu.(none)>
2012-12-31 20:21:41 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoctorSearch::Application.config.secret_token = '46ae1a74034f6588f33e3619cdbfc9d8ee910a226c177dbfd1cad8b2859f5f4d36e9606572c96224293dc90844ee84fd43503ca150507b6da98695385e5c0949'
