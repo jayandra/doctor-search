0ab398ea9d924b3380d90d17e3fb9cd86a3e9641
jp <jp@ubuntu.(none)>
2012-12-31 21:45:49 -0800

    - Test Setup
    - add rspec generator to config/application.rb
    - guard init spork  ( Guardfile, added watch for spec/support/, parameters for spork )
    - rails generate rspec:install ( .rspec, /spec, /spec/spec_helper.rb )
    - spork rspec --bootstrap ( contents in prefork: prevent from calling FG every time, comment fixtures, contents in spork.each: )
    - guard init rspec ( Guardfile, added watch for views, parameters for rspec )

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,20 @@ module DoctorSearch
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # source::  http://everydayrails.com/2012/03/12/testing-series-rspec-setup.html
+    config.generators do |g|
+       g.test_framework :rspec,
+        # generate a fixture for each model (using a Factory Girl factory, instead of an actual fixture)
+        :fixtures => true
+      #   :view_specs => false,
+      #   :helper_specs => false,
+      #   :routing_specs => false,
+      #   :controller_specs => true,
+      #   :request_specs => true
+
+      # generate factories instead of fixtures, and to save them in the spec/factories directory
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
   end
 end
