f24054882acff0117e0cdef96712e3b16e62fd89
jp <jp@ubuntu.(none)>
2012-12-31 23:36:20 -0800

    test for hospital

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,9 @@
 class Hospital < ActiveRecord::Base
   attr_accessible :address, :city, :description, :image, :name, :phone1, :phone2
+  validates_presence_of :name, :address, :city, :phone1
+  validates_numericality_of :phone1, :only_integer => true
+  validates_numericality_of :phone2, :only_integer => true, :allow_nil => true
+  validates_length_of :phone1, :phone2, :in => 6..7, :allow_nil => true
+
+  has_many :departments
 end
--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Hospital do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of (:name) }
+  it { should validate_presence_of (:address) }
+  it { should validate_presence_of (:city) }
+  it { should validate_presence_of (:phone1) }
+
+  it { should validate_numericality_of (:phone1) }
+  it { should validate_numericality_of (:phone2) }
+
+  it { should ensure_length_of(:phone1).is_at_least(6).is_at_most(7) }
+  it { should ensure_length_of(:phone2).is_at_least(6).is_at_most(7) }
+
+  it { should have_many (:departments)}
+
 end
