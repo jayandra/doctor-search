fc9937e7e5e8b9c2bb03b46e3e768aa1e22a1365
jp <jp@ubuntu.(none)>
2013-01-05 13:39:24 -0800

    test for search

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,3 +1,18 @@
 class Search < ActiveRecord::Base
   attr_accessible :department, :doctor, :hospital, :search_template
+
+  validates_presence_of :search_template
+
+  	def self.set_search_template(params)
+  		template = nil
+  		if params[:search][:doctor].present?
+			template = 'doctors'
+		elsif params[:search][:department].present? && template.nil?
+			template = 'departments'
+		else
+			template = 'hospitals'
+		end
+
+		template
+	end
 end
--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,5 +1,53 @@
 require 'spec_helper'
 
 describe Search do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should validate_presence_of (:search_template)}
+
+  describe ".set_search_template" do
+
+  	context "when doctor provided" do
+  		it "should set search_template to 'doctor'" do
+  			params = {:search => {:doctor => "doctor_name", :department => nil, :hospital => nil}}
+  			Search.set_search_template(params).should eq("doctors")
+  		end
+  	end
+  	context "when doctor and department provided" do
+  		it "should set search_template to 'doctor'" do
+  			params = {:search => {:doctor => "doctor_name", :department => "department_name", :hospital => nil}}
+  			Search.set_search_template(params).should eq("doctors")
+  		end
+  	end
+  	context "when doctor and hospital provided" do
+  		it "should set search_template to 'doctor'" do
+  			params = {:search => {:doctor => "doctor_name", :department => nil, :hospital => "hospital_name"}}
+  			Search.set_search_template(params).should eq("doctors")
+  		end
+  	end
+  	context "when doctor department and hospital provided" do
+  		it "should set search_template to 'doctor'" do
+  			params = {:search => {:doctor => "doctor_name", :department => "department_name", :hospital => "hospital_name"}}
+  			Search.set_search_template(params).should eq("doctors")
+  		end
+  	end
+
+  	context "when department provided" do
+  		it "should set search_template to 'department'" do
+  			params = {:search => {:doctor => nil, :department => "department_name", :hospital => nil}}
+  			Search.set_search_template(params).should eq("departments")
+  		end
+  	end
+  	context "when department and hospital provided" do
+  		it "should set search_template to 'department'" do
+  			params = {:search => {:doctor => nil, :department => "department_name", :hospital => "hospital_name"}}
+  			Search.set_search_template(params).should eq("departments")
+  		end
+  	end
+
+  	context "when hospital provided" do
+  		it "should set search_template to 'hospital'" do
+  			params = {:search => {:doctor => nil, :department => nil, :hospital => "hospital_name"}}
+  			Search.set_search_template(params).should eq("hospitals")
+  		end
+  	end
+  end
 end
