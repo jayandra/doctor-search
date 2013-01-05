977c13763a88d8b669b58fa63bcc052d1b728435
jp <jp@ubuntu.(none)>
2013-01-04 17:57:11 -0800

    -navbar of layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def active_link(controller, action = '')
+    	params[:controller] == controller && (action == '' || params[:action] == action) ? 'active' : ''
+  	end
 end
